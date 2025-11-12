8946947e8ece93c0795bd9ac92c69d275212d2a4
repenaattori <reimarii@oamk.fi>
2025-11-12 18:17:54 +0200

    Muutos

--- code.js
+++ code.js
@@ -5,9 +5,13 @@ let questions = [
     'Onko sanna Marin Suomen presidentti?',
     'Lasi tehdään hiekasta'
 ];
+
+
+
 let answers = ['no', 'no', 'yes'];
 
 let result = document.querySelector('#result');
+let question = document.querySelector('#question');
 let form = document.querySelector('form');
 form.addEventListener('submit', answer);
 
@@ -23,6 +27,9 @@ function answer(e){
     //Onko vastaus oikea
     if(answers[index] == a){
         points++;
+        question.classList.add('correct');
+    }else{
+         question.classList.add('incorrect');
     }
 
     //Näytetään kokonaistulos
@@ -30,15 +37,19 @@ function answer(e){
         'Sinulla on pisteitä ' + points + '/'+answers.length;
 
     index++;
-    setQuestion();
+
+    setTimeout(setQuestion, 4000);
 }
 
 function setQuestion(){
+
+    question.classList.remove('correct','incorrect');
+
     if(index > questions.length-1){
         form.classList.add('hidden');
         result.textContent = 
               'Peli päättyi. Sait ' + points + '/'+ answers.length + ' pistettä.';
     }else{
-        document.querySelector('#question').textContent = questions[index];
+        question.textContent = questions[index];
     }
 }
