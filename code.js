beb18ec637f46fd84566a5cd142d4bba4fd82162
repenaattori <reimarii@oamk.fi>
2025-11-12 19:03:28 +0200

    Laitetaan

--- code.js
+++ code.js
@@ -1,22 +1,26 @@
 let index = 0;
 let points = 0;
 let questions = [
-    'Onko html vaikeaa?', 
-    'Onko sanna Marin Suomen presidentti?',
-    'Lasi tehdään hiekasta'
+    'Mikä on yleisen ohjemointikieli selainohjelmoinnissa?',
+    'Mikä seuraavista on kitaramerkki?'
 ];
 
+let answers = [
+    ['C++','JS','Kotlin'],
+    ['Hoover','Apple', 'Fender']
+];
 
-
-let answers = ['no', 'no', 'yes'];
+let correctAnswers = ['b','c'];
 
 let result = document.querySelector('#result');
 let question = document.querySelector('#question');
 let form = document.querySelector('form');
 form.addEventListener('submit', answer);
 
+//Asetetaan ensimmäinen kysymys
 setQuestion();
 
+//Vastauksen käsittely
 function answer(e){
     e.preventDefault();
 
@@ -24,8 +28,8 @@ function answer(e){
     let fData = new FormData(form);
     let a = fData.get('answer');
 
-    //Onko vastaus oikea
-    if(answers[index] == a){
+    //Onko vastaus oikea.
+    if(correctAnswers[index] == a){
         points++;
         question.classList.add('correct');
     }else{
@@ -36,20 +40,29 @@ function answer(e){
     result.textContent = 
         'Sinulla on pisteitä ' + points + '/'+answers.length;
 
+    //Päivitetään seuraavan kysymyksen id.
     index++;
 
+    //Näytetään seuraava kysymys 4sek viiveellä.
     setTimeout(setQuestion, 4000);
 }
 
+//Kysymyksen asettaminen
 function setQuestion(){
-
+    //Poistetaan edellisen vastauksen muotoilut.
     question.classList.remove('correct','incorrect');
 
+    //Tarkistetaan loppuivatko kysymykset.
+    //Jos loppuivat, piilotetaan lomake.
     if(index > questions.length-1){
         form.classList.add('hidden');
         result.textContent = 
               'Peli päättyi. Sait ' + points + '/'+ answers.length + ' pistettä.';
     }else{
         question.textContent = questions[index];
+        let labels = document.querySelectorAll('label');
+        labels[0].textContent = answers[index][0]; 
+        labels[1].textContent = answers[index][1]; 
+        labels[2].textContent = answers[index][2]; 
     }
 }
